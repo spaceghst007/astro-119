30bd51e03ca6c14e577285f89a8f652544df6959
spaceghst007 <91283422+spaceghst007@users.noreply.github.com>
2021-09-23 12:37:03 -0700

    first homework
    this assignment uses print("") to print out my full name and preferred pronouns

---
+++ first-homework.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+#this program will write
+#my Name and preferred pronouns
+
+print("My name is Jesse Runkle") #print full name
+print("Preferred pronouns are he/him") #print preferred pronouns
