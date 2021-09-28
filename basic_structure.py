a9d9bb547b59d6aecc16e4cc4d0f07e66900c594
spaceghst007 <91283422+spaceghst007@users.noreply.github.com>
2021-09-28 14:20:14 -0700

    session_two
    These files are Python code worked on in Session 2

---
+++ basic_structure.py
@@ -0,0 +1,13 @@
+import library as alias       #imports functions for
+							  #us to use
+
+def main():				      #defines the main func
+#do some stuff
+
+#rest of the profram continues from here
+
+#if the main() function exists, fun it
+if __name__== "__main__":
+	main()
+
+#you can do other stuff down here
