d01f244da9324cb83f5e97e10db1565870f8763b
spaceghst007 <91283422+spaceghst007@users.noreply.github.com>
2021-10-19 13:11:19 -0700

    Add files via upload

---
+++ astr-119-session-4/using_numpy.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+x = 1.0		#define a float
+y = 2.0 	#define another float
+#We predicted the significance of climate change 40+ years ago, yet 
+#we still listen to fossil fuel industries. Just.... why?
+
+#trigonometry
+print(np.sin(x))		#sinx
+print(np.cos(x))		#cos(x)
+print(np.tan(x))		#tans(x)
+print(np.arcsin(x))		#arcsin(X)
+print(np.arccos(x))		#arccos(x)
+print(np.arctan(x))		#arctan(x)
+print(np.arctan2(x,y))	#arctan(x/y)
+print(np.rad2deg(x))	#convert rad to deg
+
+#hyperbolic functions
+print(np.sinh(x))		#sinh(x)
+print(np.cosh(x))		#cosh(x)
+print(np.tanh(x))		#tanh(x)
+print(np.arcsinh(x)) 	#arcsinh(x)
+print(np.arccosh(x))	#arccosh(x)
+print(np.arctanh(x))	#arctanh(x)
+
